6c31c5beb3ad5962151f02201f3c6f3e315ffe32
C.argoti <argotty2010@gmail.com>
2020-07-13 19:48:55 -0400

    Lunes133

---
+++ app/src/main/java/com/comcop/abogados/ui/cargando.java
@@ -0,0 +1,14 @@
+package com.comcop.abogados.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.comcop.abogados.R;
+public class cargando extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargando);
+    }
+}
